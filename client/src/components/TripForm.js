8190f1a9cd1a6d2a5bd01737fc1ced7ed1f5e199
Dana Lobell <danalobell@gmail.com>
2022-09-30 15:00:42 -0400

    some components updated

--- client/src/components/TripForm.js
+++ client/src/components/TripForm.js
@@ -1,7 +1,49 @@
 import React from 'react'
 
 export default function TripForm() {
+    function handleChange(e){
+
+    }
+    
+    function handleSubmit(e){
+        e.preventDefault();
+        fetch("/groups", {
+         method: "POST",
+         headers: {
+            "Content-Type": "application/json",
+         },
+         body: JSON.stringify(),
+        })
+        .then((r) => r.json())
+        .then((data) => console.log(data))
+    }
+    
   return (
-    <div>T</div>
+    <div>
+        <h1>Plan a Trip!</h1>
+        <form onSubmit={handleSubmit}>
+        <label>
+            Trip Name:
+            <input type="text" name="tripname" onChange={handleChange}/>
+        </label>
+        <br />
+        <label>
+            Destination:
+            <input type="text" name="destination"onChange={handleChange}/>
+        </label>
+        <br />
+        <label>
+            Trip Dates:
+            <input type="text" name="tripdates" onChange={handleChange}/>
+        </label>
+        <br />
+        <label>
+            Description:
+            <input type="text" name="description" onChange={handleChange}/>
+        </label>
+        <br />
+        <input type="submit"/>
+        </form>
+    </div>
   )
 }
