14503f8281ddc1912b2ecd9269a76ce8def8ccac
Dana Lobell <danalobell@gmail.com>
2022-09-30 13:39:25 -0400

    imports updated

---
+++ client/src/components/App.js
@@ -0,0 +1,16 @@
+import '../App.css';
+import Login from './login';
+import Signup from './signup';
+import TripHomePage from './triphomepage';
+import Itinerary from './itinerary';
+
+function App() {
+  return (
+    <div className="App">
+      
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/TripForm.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function TripForm() {
+  return (
+    <div>T</div>
+  )
+}
