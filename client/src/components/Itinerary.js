0df03786a1a99a5ce8ad8589d02c0413506fcbf3
Dana Lobell <danalobell@gmail.com>
2022-10-01 20:44:30 -0400

    dana branch update

--- client/src/components/Itinerary.js
+++ client/src/components/Itinerary.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react'
+import { Link } from 'react-router-dom'
 import TripDetails from './TripDetails'
 import TripCarousel from './TripCarousel'
 
