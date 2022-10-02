1c6167650d1f7c87a21738646a016e8fdf96d1d3
Dana Lobell <danalobell@gmail.com>
2022-10-02 14:04:43 -0400

    login auth

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-export default function Login() {
+export default function Login({ onLogin }) {
+  const [username, setUsername] = useState("");
+
+  function handleSubmit(e){
+    e.preventDefault();
+    fetch('/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username }),
+    })
+    .then((r) => r.json())
+    .then((user) => onLogin(user));
+  }
+  
   return (
-    <div>L</div>
+    <div>
+      <form onSubmit={handleSubmit}>
+      <label>
+            Username:
+            <input type="text" name="username" value={username} onChange={(e) => setUsername(e.target.value)}/>
+        </label>
+        <br />
+        {/* <label>
+            Password:
+            <input type="text" name="password" />
+        </label>
+        <br /> */}
+        <button type="submit">Login</button>
+        <br />
+      </form>
+      <button>Not a member?{'\n'}
+        Sign up!</button>
+    </div>
   )
 }
+
