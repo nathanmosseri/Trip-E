c4d3356e2a86023d487681c62feeaed725886e64
Nathan <namo678@me.com>
2022-10-07 10:09:40 -0400

    ordered trips

--- app/serializers/user_show_method_serializer.rb
+++ app/serializers/user_show_method_serializer.rb
@@ -1,6 +1,8 @@
 class UserShowMethodSerializer < ActiveModel::Serializer
   attributes :id, :username, :full_name, :password_digest
 
-  has_many :groups
+  has_many :groups do 
+    object.groups.order(:start_date)
+  end
 
 end
